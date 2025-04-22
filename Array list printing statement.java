3ab7695b33dd39069f67a21ee722557da46bbab6
Abinaya <abivijayakumar419@gmail.com>
2025-04-22 21:27:26 +0530

    Create Array list printing statement.java

---
+++ Array list printing statement.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+public class Main{
+  public static void main(String[] args){
+    Array List<Integer>items = new ArrayList<>();
+       items.add(1);
+       items.add(2);
+       items.add(3);
+       items.add(4);
+       items.add(5);
+    }
+ }
+for(int result:items){
+  System.out.println(result);
+}  
