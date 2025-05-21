8dce35a69321112a94129af32dc549a029ffbc28
Abinaya <abivijayakumar419@gmail.com>
2025-05-21 20:42:19 +0530

    Create Empty.java

---
+++ Empty.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main{
+  public static void main(String[] args){
+    ArrayList<Integer> numbers = new ArrayList<>();
+    if(number.isEmpty()){
+      System.out.println("The ArrayList is empty:");
+    } else {
+      System.out.println("The ArrayList is not empty:");
+    }
+    numbers.add(10);
+    if(numbers.isEmpty()){
+      System.out.println("The ArrayList is empty: ");
+    } else {
+      System.out.println("The ArrayList is not empty: ");
+    }
+  }
+}
